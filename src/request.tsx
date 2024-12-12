d811f446fa629265239f8b8ff6af84538e7b40b7
afsalcp <cpafsal66@gmail.com>
2024-12-12 15:31:56 +0530

    bug fix

--- src/request.tsx
+++ src/request.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 
-axios.defaults.baseURL=process.env.NODE_ENV=="development"?"http://192.168.1.16:8080":process.env.PUBLIC_URL;
+axios.defaults.baseURL="https://invented-polar-stomach.glitch.me";
 axios.defaults.withCredentials=true
 
 export default axios
